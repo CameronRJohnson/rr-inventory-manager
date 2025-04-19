983fa9db345ffef5553f33a121d5353a5e788c4a
Cameron Johnson <61200181+CameronRJohnson@users.noreply.github.com>
2025-04-18 18:12:08 -0600

    Last try

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : "/rr-inventory-manager"
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
