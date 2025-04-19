29820cdb8c6db05121b1ff1f0a5d4c2bccef9eb4
Cameron Johnson <61200181+CameronRJohnson@users.noreply.github.com>
2025-04-19 16:36:24 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: sveltePreprocess({
-    postcss: true
-  }),
+export default {
   kit: {
     adapter: adapter({
-      fallback: '404.html'
+      pages: 'build',
+      assets: 'build',
+      fallback: null
     }),
-
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
+      base: '/rr-inventory-manager', // important for GitHub Pages
+    },
+    trailingSlash: 'never' // <-- This is key
   }
 };
-
-export default config;
